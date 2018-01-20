44e45bae483946fd0f6e10cab67b13bd04ae0576
Mitch Kuppinger <dpneumo@gmail.com>
2018-01-20 02:15:38 +0000

    Fix the minerva postgresql user creation.

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -4,13 +4,10 @@ yum update -y
 yum -y install postgresql-server postgresql-contrib
 postgresql-setup initdb
 
-# Fix /var/lib/pgsql/data/pg_hba.conf
-#bash -c "cat ../data/pg_hba.conf >> /var/lib/pgsql/data/pg_hba.conf ;"
 cp data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
 
 systemctl start postgresql
 systemctl enable postgresql
 
-sudo -i -u postgres
-createuser --createdb --no-login --no-createrole --no-superuser "minerva"
-exit
+sudo -u postgres createuser --createdb --no-login --no-createrole --no-superuser "minerva"
+
