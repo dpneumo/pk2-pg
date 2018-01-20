38dc0077871295f81f25f2d1dc0474a7d63d1fa5
Mitchell C Kuppinger <dpneumo@gmail.com>
2018-01-19 19:35:31 -0600

    Update begore loading postgresql. Should not need sudo in the script.

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,14 +1,16 @@
-sudo yum -y install postgresql-server postgresql-contrib
-sudo postgresql-setup initdb
+################# postgresql #################
+yum update -y
+
+yum -y install postgresql-server postgresql-contrib
+postgresql-setup initdb
 
 # Fix /var/lib/pgsql/data/pg_hba.conf
 #bash -c "cat ../data/pg_hba.conf >> /var/lib/pgsql/data/pg_hba.conf ;"
-cat ../data/pg_hba.conf >> /var/lib/pgsql/data/pg_hba.conf
+cp data/pg_hba.conf >> /var/lib/pgsql/data/pg_hba.conf
 
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
+systemctl start postgresql
+systemctl enable postgresql
 
 sudo -i -u postgres
-createuser --createdb --no-login --no-createrole --no-superuser
-
+createuser --createdb --no-login --no-createrole --no-superuser "minerva"
 exit
