0c28ed61ca1d3c3f7e55640a32db18fd888e5f5d
Mitch Kuppinger <dpneumo@gmail.com>
2018-02-14 07:20:23 +0000

    Fixed remaining paths to support PG10.

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -8,14 +8,15 @@ yum -y install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x8
 echo "Installed PGDB RPM file"
 
 yum -y install postgresql10-server.x86_64 postgresql10-contrib.x86_64
-/usr/pgsql-10.0/bin/postgresql-10-setup initdb
+/usr/pgsql-10/bin/postgresql-10-setup initdb
 echo "Installed postgresql"
 
 cp data/pg_hba.conf /var/lib/pgsql/10/data/pg_hba.conf
+chown postgres:postgres /var/lib/pgsql/10/data/pg_hba.conf
 echo "Installed our pg_hba.conf"
 
-systemctl start postgresql
-systemctl enable postgresql
+systemctl start postgresql-10.service
+systemctl enable postgresql-10.service
 echo "Started and boot enabled postgresql"
 
 sudo -u postgres createuser --createdb --createrole --no-superuser "mitch"
